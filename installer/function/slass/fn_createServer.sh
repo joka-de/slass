93d67c1918c4b2ef4a38347b0a5fe85f898ef86e
PhilipJFryy <ch.wunderlich@freenet.de>
2023-12-11 18:45:54 +0100

    add server.cfg

--- installer/function/slass/fn_createServer.sh
+++ installer/function/slass/fn_createServer.sh
@@ -1,4 +1,15 @@
 fn_createServer () {
 	fn_debugMessage "fn_createServer: start" ""
+
+	while read p; do
+		if [[ $p != "" ]]; then
+			echo $p
+			. $p
+		fi
+	done < $installPath/server.cfg
+
+	fn_debugMessage "$name" ""
+	fn_debugMessage "$map" ""
+
 	fn_debugMessage "fn_createServer: end" ""
 }
