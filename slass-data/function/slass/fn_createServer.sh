281b6bb7fd4fe08e12bb566af5153c29adcafaa8
PhilipJFryy <ch.wunderlich@freenet.de>
2023-12-21 22:54:13 +0100

    add ability to read a config file and store values in an array

--- slass-data/function/slass/fn_createServer.sh
+++ slass-data/function/slass/fn_createServer.sh
@@ -1,19 +1,25 @@
 fn_createServer () {
 	fn_debugMessage "fn_createServer: start" ""	
 
-	while read line; do
-	    if [[ $line =~ [^[:space:]] ]]; then	
-		    if [[ $line =~ ^"["(.+)"]"$ ]]; then 
-		        arrname=${BASH_REMATCH[1]}
-		        declare -A $arrname
-		    elif [[ $line =~ ^([_[:alpha:]][_[:alnum:]]*)"="(.*) ]]; then 
-		        declare ${arrname}[${BASH_REMATCH[1]}]="${BASH_REMATCH[2]}"
-		    fi
-		else
-			echo ${arrname[serverName]}
-			echo ${arrname[serverPassword]}
-		fi
+	counter=0
+
+	while read line; do		
+        if [[ $line =~ ^"["(.+)"]"$ ]]; then
+        	((counter++))
+	        arrname=${BASH_REMATCH[1]}		        
+	    	declare -A $arrname
+	    elif [[ $line =~ ^([_[:alpha:]][_[:alnum:]]*)"="(.*) ]]; then 
+	        declare ${arrname}[${BASH_REMATCH[1]}]="${BASH_REMATCH[2]}"	       
+	    fi	  	
 	done < $installPath/config/server.cfg
-	 
+	
+	for (( i=1; i<=$counter; i++ )); do  
+		declare -n serverArray="server${i}"
+		echo ${serverArray[serverName]}
+		echo ${serverArray[serverPassword]}
+		echo ${serverArray[adminPassword]}
+		echo ${serverArray[mission]}
+	done
+
 	fn_debugMessage "fn_createServer: end" ""
 }
