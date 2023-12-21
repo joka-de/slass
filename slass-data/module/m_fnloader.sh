f25d5873207567abe990ae97a0e21195440d17d1
joka-de <jonny.kaars@web.de>
2023-12-21 20:36:03 +0100

    update

---
+++ slass-data/module/m_fnloader.sh
@@ -0,0 +1,14 @@
+# SLASS - fnloader
+# 
+# Author: seelenlos
+# 
+# Description:
+# loads functions
+# 
+# Parameter(s):
+# Message <String>
+# 
+# Return Value:
+# None <Any>
+#
+find $basepath/slass-data/function/fn_*.sh -type f -exec source {} \;
