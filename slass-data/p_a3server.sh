956ad212b3766c4ca9037ec9e96893746e274863
joka-de <jonny.kaars@web.de>
2023-12-21 18:16:30 +0100

    file create

---
+++ slass-data/p_a3server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#
+#
+# SLASS - a3server
+# 
+# Author: seelenlos
+# 
+# Description:
+# starts and stops a server instance with config file
+# 
+# Parameter(s): {i} {startparameters_{j}.scfg}
+# Message <string>
+# 
+# Return Value:
+# None <Any>
+#
