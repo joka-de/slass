53f93243532cf0f325cf13f427cc213db1981a08
joka-de <jonny.kaars@web.de>
2023-12-21 18:35:00 +0100

    update

--- slass-data/module/m_mka3filestructure.sh
+++ slass-data/module/m_mka3filestructure.sh
@@ -20,3 +20,5 @@ mkdir $basepath/a3/a3srv${serverid} --mode=775
 ln -s ${a3instdir}/a3/a3master/* $basepath/a3/a3srv${serverid}/
 rm -f $basepath/a3/a3srv${serverid}/keys
 mkdir $basepath/a3/a3srv${serverid}/keys --mode=775
+rm -f $basepath/a3/a3srv${serverid}/cfg
+mkdir $basepath/a3/a3srv${serverid}/cfg --mode=775
