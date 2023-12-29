9fe6909d5de8d6f691d3f78aa4dc7ee65b991611
joka-de <jonny.kaars@web.de>
2023-12-29 15:07:13 +0100

    update

--- _oldversions/fn_createServer.sh
+++ _oldversions/fn_createServer.sh
@@ -13,21 +13,12 @@ fn_createServer () {
 	    fi	  	
 	done < $installPath/config/server.cfg
 	
-	for (( i=1; i<=$counter; i++ )); do
+	for (( i=1; i<=$counter; i++ )); do  
 		declare -n serverArray="server${i}"
-		: '
-		ln -s ${installPath}/a3/a3master/* $installPath/a3srv${i}/
-
-		if [[ ${serverArray[headlessClient]} -gt 0 ]]; then
-			for (( n=1; n<=${serverArray[headlessClient]}; n++ )); do
-				ln -s ${installPath}/a3/a3master/* $installPath/a3srv${i}_hc${n}/
-			done
-		fi
-		'
 		echo ${serverArray[serverName]}
 		echo ${serverArray[serverPassword]}
 		echo ${serverArray[adminPassword]}
-		echo ${serverArray[mission]}		
+		echo ${serverArray[mission]}
 	done
 
 	fn_debugMessage "fn_createServer: end" ""
